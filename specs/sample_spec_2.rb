1b2be0ad3a95c577532d247dcf3fcf2e1b25db5e
carlad <carlad@users.noreply.github.com>
2017-10-14 17:50:48 +0200

    Delete sample_spec_2.rb

--- specs/sample_spec_2.rb
+++
@@ -1,8 +0,0 @@
-require "rspec"
-
-RSpec.describe "A sample test" do
-  it "runs aok" do
-    expect(42).to eql("the meaning of life, the universe, and everything")
-    puts "second test"
-  end
-end
