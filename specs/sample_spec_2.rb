f43ad2f18d8d6262cea49c0d1f4cf72dd45c5248
carlad <carlad@users.noreply.github.com>
2017-10-14 17:49:51 +0200

    Create sample_spec_2.rb

---
+++ specs/sample_spec_2.rb
@@ -0,0 +1,8 @@
+require "rspec"
+
+RSpec.describe "A sample test" do
+  it "runs aok" do
+    expect(42).to eql("the meaning of life, the universe, and everything")
+    puts "second test"
+  end
+end
